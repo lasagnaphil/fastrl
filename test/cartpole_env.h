b66fc40edc17b6cddeb5bbcc877fb1effd1fd4cd
Phillip Chang <phil@mrl.snu.ac.kr>
2021-01-09 21:41:55 +0900

    Add CartPole (continuous)

---
+++ test/cartpole_env.h
@@ -0,0 +1,115 @@
+//
+// Created by lasagnaphil on 21. 1. 8..
+//
+
+#ifndef FASTRL_CARTPOLE_ENV_H
+#define FASTRL_CARTPOLE_ENV_H
+
+#include <random>
+#include <tuple>
+#include <raylib.h>
+
+inline float angle_normalize(float x) {
+    return std::fmod(x + (float)M_PI, 2*(float)M_PI) - (float)M_PI;
+}
+
+#define SQUARE(x) ((x)*(x))
+
+// A continuous version of the famous CartPole environment.
+struct CartpoleEnv {
+    constexpr static int obs_dim = 4;
+    constexpr static int act_dim = 1;
+
+    float gravity = 9.8f;
+    float masscart = 1.0f;
+    float masspole = 0.1f;
+    float total_mass = 1.0f + 0.1f;
+    float length = 0.5f;
+    float polemass_length = 0.1f + 0.5f;
+    float force_mag = 10.0f;
+    float tau = 0.02f;
+    float theta_threshold_radians = 12.f * 2.f * M_PI / 360;
+    float x_threshold = 2.4f;
+
+    int steps_beyond_done = -1;
+
+    int screen_width = 800;
+    int screen_height = 600;
+
+    std::array<float, obs_dim> state;
+    std::default_random_engine random_engine;
+
+    CartpoleEnv() {
+        seed();
+        reset();
+    }
+
+    void seed(unsigned long seed = 0) {
+        random_engine.seed(seed);
+    }
+
+    std::tuple<std::array<float, obs_dim>, float, bool> step(float action) {
+        auto [x, x_dot, theta, theta_dot] = state;
+        float force = std::min(std::max(action, -force_mag), force_mag);
+        float costheta = std::cos(theta);
+        float sintheta = std::sin(theta);
+
+        float temp = (force + polemass_length * SQUARE(theta_dot) * sintheta) / total_mass;
+        float thetaacc = (gravity * sintheta - costheta * temp) /
+                (length * (4.0f / 3.0f - masspole * SQUARE(costheta) / total_mass));
+        float xacc = temp - polemass_length * thetaacc * costheta / total_mass;
+
+        x += tau * x_dot;
+        x_dot += tau * xacc;
+        theta += tau * theta_dot;
+        theta_dot += tau * thetaacc;
+
+        state = {x, x_dot, theta, theta_dot};
+
+        bool done = x < -x_threshold || x > x_threshold || theta < -theta_threshold_radians || theta > theta_threshold_radians;
+        float reward;
+        if (!done) {
+            reward = 1.0f;
+        }
+        else if (steps_beyond_done == -1) {
+            steps_beyond_done = 0;
+            reward = 1.0f;
+        }
+        else {
+            if (steps_beyond_done == 0) {
+                fprintf(stderr, "You are calling 'step()' even though this "
+                                "environment has already returned done = True. You "
+                                "should always call 'reset()' once you receive 'done = "
+                                "True' -- any further steps are undefined behavior.\n");
+            }
+            steps_beyond_done++;
+            reward = 0.0f;
+        }
+        return {state, reward, done};
+    }
+
+    std::array<float, obs_dim> reset() {
+        for (int i = 0; i < 4; i++) {
+            state[i] = std::uniform_real_distribution<float>(-0.05, 0.05)(random_engine);
+        }
+        steps_beyond_done = -1;
+        return state;
+    }
+
+    void render() {
+        auto [x, x_dot, theta, theta_dot] = state;
+        float scale = 400;
+        float pole_width = 20;
+        float pole_height = scale * length;
+        float pos_x = screen_width*0.5f - pole_width*0.5f + scale*x;
+        float pos_y = screen_height*0.5f;
+        Rectangle pole_rect {pos_x, pos_y, pole_width, pole_height};
+        Vector2 pole_origin {pole_width*0.5f, 0};
+        DrawRectanglePro(pole_rect, pole_origin, RAD2DEG * (theta + M_PI), RED);
+        DrawCircle(pos_x, pos_y, pole_width, GRAY);
+    }
+
+};
+
+
+#endif //FASTRL_CARTPOLE_ENV_H
---
+++ test/test_cartpole_env.cpp
@@ -0,0 +1,153 @@
+//
+// Created by lasagnaphil on 21. 1. 3..
+//
+
+#define USE_RENDERER
+
+#include "cartpole_env.h"
+#include "fastrl/fastrl.h"
+
+int main(int argc, char** argv) {
+    // google::InitGoogleLogging(argv[0]);
+    // google::SetStderrLogging(google::ERROR);
+
+    auto device = torch::kCPU;
+    int num_envs = 20;
+    bool eval_enabled = true;
+
+    auto policy_opt = fastrl::PolicyOptions();
+    policy_opt.actor_hidden_dim = {256, 256};
+    policy_opt.critic_hidden_dim = {256, 256};
+    policy_opt.activation_type = fastrl::NNActivationType::Tanh;
+    policy_opt.device = device;
+
+    auto rb_opt = fastrl::RolloutBufferOptions();
+    rb_opt.gae_lambda = 0.9f;
+    rb_opt.gamma = 0.95f;
+    rb_opt.buffer_size = 512;
+    rb_opt.num_envs = num_envs;
+
+    auto ppo_opt = fastrl::PPOOptions();
+    ppo_opt.learning_rate = 5e-5f;
+    ppo_opt.num_epochs = 10;
+    ppo_opt.clip_range_vf_enabled = false;
+    ppo_opt.device = device;
+
+    int sgd_minibatch_size = 64;
+
+    using MyEnv = CartpoleEnv;
+
+    auto logger = std::make_shared<TensorBoardLogger>("logs/tfevents.pb");
+    auto policy = std::make_shared<fastrl::Policy>(MyEnv::obs_dim, MyEnv::act_dim, policy_opt);
+    auto rollout_buffer = fastrl::RolloutBuffer(MyEnv::obs_dim, MyEnv::act_dim, rb_opt);
+    auto ppo = fastrl::PPO(ppo_opt, policy, logger);
+
+    std::vector<MyEnv> env(num_envs);
+    MyEnv eval_env;
+
+    int max_steps = 1000;
+    for (int step = 1; step <= max_steps; step++) {
+        std::vector<float> last_values(num_envs);
+        std::vector<int8_t> last_dones(num_envs);
+
+        policy->eval();
+        for (int e = 0; e < num_envs; e++) {
+            auto obs = env[e].reset();
+            bool done;
+            for (int i = 0; i < rb_opt.buffer_size; i++) {
+                auto obs_tensor = torch::from_blob(obs.data(), {(int)obs.size()}).to(device);
+                auto [action_dist, value_tensor] = policy->forward(obs_tensor);
+                float value = value_tensor.item<float>();
+                float action = action_dist.sample().item<float>();
+                float log_prob = action_dist.log_prob(obs_tensor).item<float>();
+                auto [new_obs, reward, new_done] = env[e].step(action);
+                rollout_buffer.add(e, obs.data(), &action, reward, done, value, log_prob);
+                obs = new_obs;
+                done = new_done;
+                if (done) {
+                    obs = env[e].reset();
+                }
+            }
+
+            {
+                torch::NoGradGuard guard {};
+                auto obs_tensor = torch::from_blob(obs.data(), obs.size()).to(device);
+                auto [_, value_tensor] = policy->forward(obs_tensor);
+                last_values[e] = value_tensor.item<float>();
+                last_dones[e] = (int8_t)done;
+            }
+        }
+
+        rollout_buffer.compute_returns_and_advantage(last_values.data(), last_dones.data());
+        float avg_episode_reward = rollout_buffer.get_average_episode_reward();
+        logger->add_scalar("train/avg_episode_reward", ppo.iter, avg_episode_reward);
+        std::printf("Average reward: %f\n", avg_episode_reward);
+
+        auto batches = rollout_buffer.get_samples(sgd_minibatch_size);
+
+        policy->train();
+        ppo.train(batches.data(), batches.size());
+        rollout_buffer.reset();
+
+        if (step % 10 == 0) {
+            // Save model
+            torch::serialize::OutputArchive output_archive;
+            policy->save(output_archive);
+            output_archive.save_to(std::string("policy_") + std::to_string(step) + ".pt");
+
+            // Evaluation
+            if (eval_enabled) {
+
+#ifdef USE_RENDERER
+                InitWindow(800, 600, "PendulumV0");
+                SetTargetFPS(60);
+
+                policy->eval();
+                auto obs = eval_env.reset();
+                auto done = false;
+                int num_episodes = 0;
+                float avg_episode_reward = 0.0f;
+                float episode_reward = 0.0f;
+                int time = 0;
+
+                while (!WindowShouldClose()) {
+                    torch::NoGradGuard guard {};
+                    auto obs_tensor = torch::from_blob(obs.data(), {(int)obs.size()}).to(device);
+                    auto [action_dist, value_tensor] = policy->forward(obs_tensor);
+                    float action = action_dist.sample().item<float>();
+                    auto [new_obs, reward, new_done] = eval_env.step(action);
+                    // auto [new_obs, reward, new_done] = eval_env.step(-1.f * (eval_env.state[0]) - 1.f * (eval_env.state[1]));
+                    // std::printf("State: [%f %f %f]\n", obs[0], obs[1], obs[2]);
+                    // std::printf("Reward: %f\n", reward);
+                    episode_reward += reward;
+                    obs = new_obs;
+                    done = new_done;
+                    if (done) {
+                        num_episodes++;
+                        avg_episode_reward += episode_reward;
+                        episode_reward = 0.0f;
+                        eval_env.reset();
+                    }
+
+                    BeginDrawing();
+                    ClearBackground(RAYWHITE);
+                    eval_env.render();
+                    DrawFPS(10, 10);
+                    DrawText(TextFormat("Reward: %f", reward), 10, 40, 20, DARKGRAY);
+                    DrawText(TextFormat("Action: %f", action), 10, 70, 20, DARKGRAY);
+                    EndDrawing();
+                    time++;
+                    if (time == 200) break;
+                }
+
+                avg_episode_reward /= num_episodes;
+                std::printf("Average eval reward: %f\n", avg_episode_reward);
+
+                CloseWindow();
+#endif
+            }
+        }
+    }
+
+    return 0;
+}
